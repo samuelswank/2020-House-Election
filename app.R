9b7196162df0e017fe9cfb51cf891e6cb4730f0f
Samuel Swank <samswank@gmail.com>
2021-01-09 13:57:46 -0700

    dynamic dropdown

--- app.R
+++ app.R
@@ -1,43 +1,31 @@
 library(shiny)
-library(tidycensus)
-library(tigris)
 library(tidyverse)
-source("dataWrangling/houseResults.R")
-source("map.R")
-
+source("census.R")
 
 ui <- fluidPage(
   fluidRow(
-      column(
-          4, 
-          selectInput(
-              "selectedState", 
-              "State", 
-              choices = districtChoices,
-              selected = "Utah District 4"
-              )
-          )
-  ),
-  fluidRow(
-    column(3, plotOutput("stateMap")),
-    column(3, plotOutput("districtMap"))
+      column(4, selectInput("selectedState", "State", choices = state.name)),
+      column(4, uiOutput("selectedDistrict"))
   )
 )
 
 server <- function(input, output, session) {
-    output$stateMap <- renderPlot({
-      plotState(
-        stateData(input$selectedDistrict, stateString(input$selectedDistrict)),
-        input$selectedDistrict
-        )
-    })
-
-    output$districtMap <- renderPlot({
-      plotDistrict(
-        stateData(input$selectedDistrict, stateString(input$selectedDistrict)),
-        input$selectedDistrict
-        )
-    })
+  output$selectedDistrict <- renderUI({
+    selectInput("selectedDistrict", "District", choices = districtChoices[[input$selectedState]])
+  })
+    # output$stateMap <- renderPlot({
+    #   plotState(
+    #     stateData(input$selectedDistrict, stateString(input$selectedDistrict)),
+    #     input$selectedDistrict
+    #     )
+    # })
+    # 
+    # output$districtMap <- renderPlot({
+    #   plotDistrict(
+    #     stateData(input$selectedDistrict, stateString(input$selectedDistrict)),
+    #     input$selectedDistrict
+    #     )
+    # })
 }
 
 shinyApp(ui, server)
