02cb6a02fa2ae9aede2ee0e4c2fa857f6f1b87bc
Samuel Swank <samswank@gmail.com>
2021-01-09 17:21:52 -0700

    simplified functional input, solved Alaska sf

--- map.R
+++ map.R
@@ -1,58 +1,84 @@
 library(tidyverse)
-source("houseResults.R")
-source("stringManipulator.R")
+source("dataWrangling/houseResults.R")
 
 cd117 <- tigris::congressional_districts(year = 2019)
 
-stateData <- function(selectedDistrict, stateString) {
-  stateName <- stateString(selectedDistrict)
-  stateParty <- flippedResults %>% filter(fipsCode == fipsList[[stateName]]$st)
+stateData <- function(selectedState) {
+  stateParty <- hr %>%
+    filter(fipsCode == fipsList[[selectedState]]$st) %>%
+    select(c(3, 4, 6, 11))
+  
+  if (selectedState %in% atLarge) {
+    stateParty$district[1] <- "Congressional District (at Large)"
+  }
 
-  colnames(stateParty)[3] <- "STATEFP"
-  colnames(stateParty)[4] <- "NAMELSAD"
+  colnames(stateParty)[1] <- "STATEFP"
+  colnames(stateParty)[2] <- "NAMELSAD"
 
   shapeFile <- cd117  %>%
-    filter(STATEFP == fipsList[[stateName]]$st) %>%
+    filter(STATEFP == fipsList[[selectedState]]$st) %>%
     merge(stateParty, by = c("STATEFP", "NAMELSAD"))
   
   return(shapeFile)
 }
 
-districtData <- function(stateData, selectedDistrict) {
-  return(
-    stateData %>% 
-      subset(str_detect(NAMELSAD, districtString(selectedDistrict)) == TRUE)
-    )
+districtData <- function(selectedState, selectedDistrict) {
+  return(stateData(selectedState) %>% filter(NAMELSAD == selectedDistrict))
 }
 
-plotState <- function(stateData, selectedDistrict) {
-  ggplot() + 
-    geom_sf(
-      stateData, mapping = aes(fill = party), size = 0.75, color = "black"
+plotState <- function(selectedState) {
+  if (selectedState == "Alaska") {
+    ggplot() + 
+      geom_sf(
+        stateData(selectedState),
+        mapping = aes(fill = party),
+        size = 0.75,
+        color = "black"
       ) +
-    scale_fill_manual(values = c("R" = "#D20F26", "D" = "#1B4E81")) +
-    ggtitle(stateString(selectedDistrict)) +
-    theme(
-      panel.background = element_blank(),
-      plot.title = element_text(
-        hjust = 0.5, size = 24, family = "NewCenturySchoolbook"
+      xlim(-180, -120) +
+      scale_fill_manual(values = c("R" = "#D20F26", "D" = "#1B4E81")) +
+      ggtitle(selectedState) +
+      theme(
+        panel.background = element_blank(),
+        plot.title = element_text(
+          hjust = 0.5, size = 24, family = "NewCenturySchoolbook"
         ),
-      axis.text = element_blank(),
-      axis.ticks = element_blank(),
-      legend.position = "none"
-    )
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.position = "none"
+      )
+  } else {
+    ggplot() + 
+      geom_sf(
+        stateData(selectedState),
+        mapping = aes(fill = party),
+        size = 0.75,
+        color = "black"
+      ) +
+      scale_fill_manual(values = c("R" = "#D20F26", "D" = "#1B4E81")) +
+      ggtitle(selectedState) +
+      theme(
+        panel.background = element_blank(),
+        plot.title = element_text(
+          hjust = 0.5, size = 24, family = "NewCenturySchoolbook"
+        ),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.position = "none"
+      )
+    }
 }
 
-plotDistrict <- function(stateData, selectedDistrict) {
+plotDistrict <- function(selectedState, selectedDistrict) {
   ggplot() + 
     geom_sf(
-      districtData(stateData, selectedDistrict),
+      districtData(selectedState, selectedDistrict),
       mapping = aes(fill = party), 
       size = 0.75, 
       color = "black"
       ) +
     scale_fill_manual(values = c("R" = "#D20F26", "D" = "#1B4E81")) +
-    labs(title = paste("District", districtString(selectedDistrict))) +
+    labs(title = selectedDistrict) +
     theme(
       panel.background = element_blank(),
       plot.title = element_text(
