120f38d5f11a0ee4fdbdb04376bfbbb0896883b7
Samuel Swank <samswank@gmail.com>
2021-01-30 18:53:40 -0700

    partyPreds + plotPredicted

--- map.R
+++ map.R
@@ -2,15 +2,15 @@ library(tidyverse)
 source("dataWrangling/houseResults.R")
 
 cd117 <- tigris::congressional_districts(year = 2019)
-partyPreds <- read_csv(
-  "data/predictions/partyPreds.csv",
-  col_names = c("NAMELSAD", "predicted", "actual")
-  )[, 1:2]
+partyPreds <- read_csv("data/predictions/partyPreds.csv")
+partyPreds$district <- partyPreds$district %>%
+  sapply(function(x) gsub( " *\\(.*?\\) *", " 1", x))
 
 stateData <- function(selectedState) {
   stateParty <- hr %>%
     filter(fipsCode == fipsList[[selectedState]]$st) %>%
-    select(c(3, 4, 6, 11))
+    merge(partyPreds, by = c("state", "district")) %>%
+    select(4, 2, 6, 12)
   
   if (selectedState %in% atLarge) {
     stateParty$district[1] <- "Congressional District (at Large)"
@@ -30,11 +30,6 @@ districtData <- function(selectedState, selectedDistrict) {
   return(stateData(selectedState) %>% filter(NAMELSAD == selectedDistrict))
 }
 
-predictionData <- function(selectedState, selectedDistrict) {
-  districtData <- districtData(selectedState, selectedDistrict)
-  return(districtData)
-}
-
 plotState <- function(selectedState) {
   if (selectedState == "Alaska") {
     ggplot() + 
@@ -46,7 +41,7 @@ plotState <- function(selectedState) {
       ) +
       xlim(-180, -120) +
       scale_fill_manual(values = c("R" = "#D20F26", "D" = "#1B4E81")) +
-      ggtitle(selectedState) +
+      ggtitle("Actual") +
       theme(
         panel.background = element_blank(),
         plot.title = element_text(
@@ -65,7 +60,7 @@ plotState <- function(selectedState) {
         color = "black"
       ) +
       scale_fill_manual(values = c("R" = "#D20F26", "D" = "#1B4E81")) +
-      ggtitle(selectedState) +
+      ggtitle("Actual") +
       theme(
         panel.background = element_blank(),
         plot.title = element_text(
@@ -97,4 +92,68 @@ plotDistrict <- function(selectedState, selectedDistrict) {
       axis.ticks = element_blank(),
       legend.position = "none"
       )
+}
+
+plotPredicted <- function(selectedState, selectedDistrict, geography) {
+  if (is.na(selectedDistrict) == TRUE & geography == "state") {
+    if (selectedState == "Alaska") {
+      ggplot() + 
+        geom_sf(
+          stateData(selectedState),
+          mapping = aes(fill = predicted),
+          size = 0.75,
+          color = "black"
+        ) +
+        xlim(-180, -120) +
+        scale_fill_manual(values = c("R" = "#D20F26", "D" = "#1B4E81")) +
+        ggtitle("Predicted") +
+        theme(
+          panel.background = element_blank(),
+          plot.title = element_text(
+            hjust = 0.5, size = 24, family = "NewCenturySchoolbook"
+          ),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          legend.position = "none"
+        )
+    } else {
+      ggplot() + 
+        geom_sf(
+          stateData(selectedState),
+          mapping = aes(fill = predicted),
+          size = 0.75,
+          color = "black"
+        ) +
+        scale_fill_manual(values = c("R" = "#D20F26", "D" = "#1B4E81")) +
+        ggtitle("Predicted") +
+        theme(
+          panel.background = element_blank(),
+          plot.title = element_text(
+            hjust = 0.5, size = 24, family = "NewCenturySchoolbook"
+          ),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          legend.position = "none"
+        )
+    }
+  } else if (geography == "district") {
+    ggplot() + 
+      geom_sf(
+        districtData(selectedState, selectedDistrict),
+        mapping = aes(fill = predicted), 
+        size = 0.75, 
+        color = "black"
+      ) +
+      scale_fill_manual(values = c("R" = "#D20F26", "D" = "#1B4E81")) +
+      labs(title = selectedDistrict) +
+      theme(
+        panel.background = element_blank(),
+        plot.title = element_text(
+          hjust = 0.5, size = 18, family = "NewCenturySchoolbook"
+        ),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.position = "none"
+      )
+  }
 }
