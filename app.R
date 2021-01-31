f9a65824294faa63f758cf46d52ab97ccd689a83
Samuel Swank <samswank@gmail.com>
2021-01-30 18:53:09 -0700

    added predictions maps

--- app.R
+++ app.R
@@ -1,7 +1,6 @@
 library(shiny)
 library(tidyverse)
 source("stringManipulator.R")
-source("census.R")
 source("map.R")
 
 stateChoices <- state.name %>% R.utils::insert(1, c(""))
@@ -17,6 +16,10 @@ ui <- fluidPage(
   ),
   fluidRow(
     column(4, plotOutput("stateMap")), column(4, plotOutput("districtMap"))
+    ),
+  fluidRow(
+    column(4, plotOutput("predictedState")),
+    column(4, plotOutput("predictedDistrict"))
     )
 )
 
@@ -31,15 +34,29 @@ server <- function(input, output, session) {
   # output$stateMap <- renderPlot({plotState(input$selectedState)})
   
   observeEvent(input$selectedState, {
-    if (input$selectedState == "") {output$stateMap <- NULL}
-    else if (input$selectedState %!in% atLarge) {
+    if (input$selectedState == "") {
+      output$stateMap     <- NULL
+      output$predictedMap <- NULL
+    } else if (input$selectedState %!in% atLarge) {
       output$stateMap <- renderPlot({plotState(input$selectedState)})
       output$districtMap <- renderPlot({
         plotDistrict(input$selectedState, input$selectedDistrict)
+        })
+      output$predictedState <- renderPlot({
+        plotPredicted(input$selectedState, NA, geography = "state")
+        })
+      output$predictedDistrict <- renderPlot({
+        plotPredicted(
+          input$selectedState, input$selectedDistrict, geography = "district"
+          )
       })
     } else if (input$selectedState %in% atLarge) {
       output$stateMap <- renderPlot({plotState(input$selectedState)})
       output$districtMap <- NULL
+      output$predictedState <- renderPlot({
+        plotPredicted(input$selectedState, NA, geography = "state")
+      })
+      output$predictedDistrict <- NULL
     }
   })
 }
