3c8693a3e7a423d78d441dfb9b67a36c7c227835
Samuel Swank <samswank@gmail.com>
2021-02-27 19:31:11 -0700

    acceptable model

--- flippedModel.R
+++ flippedModel.R
@@ -12,9 +12,9 @@ flippedScaled <- dfScaled[
 flipped    <- length((flippedScaled %>% subset(flipped == TRUE))$flipped)
 notFlipped <- length((flippedScaled %>% subset(flipped == FALSE))$flipped)
 
-set.seed(42)
+set.seed(19)
 propSample <- sample(
-  1:nrow(flippedScaled %>% filter(party == "D" & flipped == FALSE)), 87
+  1:nrow(flippedScaled %>% filter(party == "D" & flipped == FALSE)), 37
 )
 
 flippedScaled$district <- rownames(flippedScaled)
@@ -30,8 +30,8 @@ propFlipped <- propFlipped[, !names(propFlipped) %in% c("party"), drop = F]
 
 rownames(propFlipped) <- NULL
 
-trainProp <- propFlipped[1:80, ]
-testProp  <- propFlipped[81:100, ]
+trainProp <- propFlipped[1:40, ]
+testProp  <- propFlipped[41:50, ]
 
 trainProp$flipped <- as.character(trainProp$flipped)
 testProp$flipped <- as.character(testProp$flipped)
@@ -39,36 +39,9 @@ testProp$flipped <- as.character(testProp$flipped)
 trainProp$flipped <- factor(trainProp$flipped, levels = c("TRUE", "FALSE"))
 testProp$flipped <- factor(testProp$flipped, levels = c("TRUE", "FALSE"))
 
-for (i in 1:25) {
-  set.seed(1)
-  treeModel <- randomForest(flipped ~ ., data = trainProp, ntree = i)
-  sprintf("%d\n-----------------------------------------------------\n", i)
-  flipped.train.preds <- data.frame(
-    district = trainProp$district,
-    predicted = propModel$predicted,
-    actual = trainProp$flipped
-  )
-
-  train.flipped.cm <- yardstick::conf_mat(
-    flipped.train.preds, truth = actual, estimate = predicted
-  )
-
-  print(i)
-  print(
-    summary(train.flipped.cm) %>%
-      subset(
-        .metric == "accuracy" |
-          .metric == "mcc" |
-          .metric == "precision" |
-          .metric == "recall" |
-          .metric == "f_meas"
-      )
-  )
-}
-
 set.seed(42)
 propModel <- randomForest(
-  flipped ~ ., data = trainProp %>% select(1:65), ntree = 1
+  flipped ~ ., data = trainProp %>% select(1:65), ntree = 25
   )
 
 flipped.train.preds <- data.frame(
