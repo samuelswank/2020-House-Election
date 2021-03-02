4b81d658e87459a23f1ed19914234e3361240435
Samuel Swank <samswank@gmail.com>
2021-03-02 10:43:35 -0700

    removed flippedModel

--- app.R
+++ app.R
@@ -2,7 +2,6 @@ library(shiny)
 library(tidyverse)
 source("stringManipulator.R")
 source("map.R")
-source("flippedModel.R")
 
 stateChoices <- state.name %>% R.utils::insert(1, c(""))
 '%!in%' <- function(x,y){!('%in%'(x,y))}
@@ -11,22 +10,19 @@ ui <- fluidPage(
   # Dropdown Menu Row
   fluidRow(
     column(
-      4, 
+      6, 
       selectInput("selectedState", "State", choices = stateChoices)
       ),
-    column(4, uiOutput("selectedDistrict"))
+    column(6, uiOutput("selectedDistrict"))
   ),
   # Actual Row
   fluidRow(
-    column(4, uiOutput("party"), plotOutput("stateMap")),
-    column(4, uiOutput("filler"), plotOutput("districtMap")),
-    column(4, uiOutput("flipped"), uiOutput("flippedActual"))
+    column(6, plotOutput("stateMap")), column(6, plotOutput("districtMap"))
     ),
   # Predicted Row
   fluidRow(
-    column(4, plotOutput("predictedState")),
-    column(4, plotOutput("predictedDistrict")),
-    column(4, uiOutput("flippedPredicted"))
+    column(6, plotOutput("predictedState")),
+    column(6, plotOutput("predictedDistrict"))
     )
 )
 
@@ -63,73 +59,6 @@ server <- function(input, output, session) {
         plotPredicted(input$selectedState, NA, geography = "state")
       })
       output$predictedDistrict <- NULL
-    }
-  })
-  
-  # flippedText helper function
-  flippedText <- function(resultString) {
-    renderUI({
-      tags$div(
-        style = "margin: 0; position: absolute; top: 300%;",
-        tags$b(style = "font-size: 25px", resultString)
-      )
-    })
-  }
-  
-  
-  # Flipped District Model
-  observeEvent(input$selectedDistrict, {
-    if (
-      input$selectedDistrict == "" |
-      is.null(reverseDistrict(input$selectedState, input$selectedDistrict)) == TRUE
-      ) {
-      output$party            <- NULL
-      output$filler           <- NULL
-      output$flipped          <- NULL
-      output$flippedActual    <- NULL
-      output$flippedPredicted <- NULL
-    } else if (
-      reverseDistrict(input$selectedState, input$selectedDistrict) %!in%
-      flipped.preds$district
-      ) {
-      output$party            <- NULL
-      output$filler           <- NULL
-      output$flipped          <- NULL
-      output$flippedActual    <- NULL
-      output$flippedPredicted <- NULL
-    } else {
-      if (flippedResult(input$selectedState, input$selectedDistrict)$in_sample == FALSE) {
-        output$flippedAcutal   <- NULL
-        output$flippedPreicted <- NULL
-      } else {
-        output$party <- renderUI({
-          tags$div(
-            style = "text-align: center", tags$h2("Party Affiliation Model")
-            )
-          })
-        output$filler  <- renderUI({
-          h2("----------------------------------------------------")
-          })
-        output$flipped   <- renderUI({
-          tags$div(
-            style = "text-align: left", tags$h2("Flipped Model")
-          )
-        })
-        
-        # Flipped Actual Text
-        if (flippedResult(input$selectedState, input$selectedDistrict)$actual == TRUE) {
-          output$flippedActual <- flippedText("FLIPPED REPUBLICAN")
-        } else {
-          output$flippedActual <- flippedText("STAYED DEMOCRAT")
-        }
-        
-        # Flipped Predicted Text
-        if (flippedResult(input$selectedState, input$selectedDistrict)$predicted == TRUE) {
-          output$flippedPredicted <- flippedText("FLIPPED REPUBLICAN")
-        } else {
-          output$flippedPredicted <- flippedText("STAYED DEMOCRAT")
-        }
-      }
     }
   })
 }
