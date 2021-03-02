4769db6ad19e8712d340bc5aae02a88fb3bca07f
Samuel Swank <samswank@gmail.com>
2021-03-02 15:12:10 -0700

    basic pieChart function

---
+++ statistics.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+
+# source("")
+
+# Relevant Statistics
+
+# Walking/Public Transit
+# Renter Occupied Units
+# Natural-born Citizens
+# Commuters by Car
+# Owner Occupied Units
+# Foreign-born Citizens
+# Whites
+# Asians
+# Persons of Other Races
+# Median Rent
+
+modelData <- read_csv("data/census/demographics/preprocessed/modelData.csv")
+
+pieChart <- function(selectedState, selectedDistrict, categories, n_seed = NULL) {
+  
+  c <- c()
+  for (i in 1:length(categories)) {
+    c[i] <- (modelData %>%
+      filter(
+        districtDemographics == reverseDistrict(selectedState, selectedDistrict)
+        ) %>%
+      select(categories) %>%
+      .[[categories[i]]]) / 100
+  }
+
+  if (is.null(n_seed) == TRUE) {set.seed(n_seed)}
+  
+  sampleVec <- sample(categories, 710767, replace = TRUE, prob = c)
+  counts <- table(sampleVec) %>% as.data.frame()
+  
+  ggplot(data = counts, aes(x = "", y = Freq, fill = sampleVec)) +
+    geom_bar(stat = "identity", width = 1) +
+    coord_polar("y", start = 0)
+}
