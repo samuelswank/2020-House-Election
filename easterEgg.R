f949f2df911955f93b5e65800e35e1b8b890dc30
Samuel Swank <samswank@gmail.com>
2021-03-09 14:20:23 -0700

    eeTitle h1 + eeText

--- easterEgg.R
+++ easterEgg.R
@@ -4,7 +4,7 @@ centerText <- function(htmlText) {
   return(tags$div(style = "text-align: center;", htmlText))
 }
 
-eeTitle <- centerText("A Note From the Creator")
+eeTitle <- centerText(h1("A Note From the Creator"))
 
 eeText <- tags$body(
   tags$p("This web applicaiton was originally intended to explore what
